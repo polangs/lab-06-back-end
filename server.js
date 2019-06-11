f654f150e106f2452d0644e8b80923b0236e008a
EvanBC1 <starfoxz13@gmail.com>
2019-06-11 13:18:07 -0700

    step 2

--- server.js
+++ server.js
@@ -27,22 +27,23 @@ app.get('/location', (request,response) => {
 // Helper Functions
 function searchToLatLong(query) {
   const geoData = require('./data/geo.json');
-  const location = {
-    search_query: query,
-    formatted_query: geoData.results[0].formatted_address,
-    latitude: geoData.results[0].geometry.location.lat,
-    longitude: geoData.results[0].geometry.location.lng
-  };
+  const location = new Location (query, geoData);
+  // const location = {
+  //   search_query: query,
+  //   formatted_query: geoData.results[0].formatted_address,
+  //   latitude: geoData.results[0].geometry.location.lat,
+  //   longitude: geoData.results[0].geometry.location.lng
+  // };
   return location;
 }
 
 // Refactor the searchToLatLong function to replace the object literal with a call to this constructor function:
-// function Location(query, geoData) {
-//   this.search_query = query;
-//   this.formatted_query = geoData.result[0].formatted_address;
-//   this.latitude = geoData.results[0].geometry.location.lat;
-//   this.longitude = geoData.results[0].geometry.location.lng;
-// }
+function Location(query, geoData) {
+  this.search_query = query;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`) );
